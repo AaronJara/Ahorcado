4bcd2901a7698fc1255294e6a50beb1a45743633
AaronJara <aaronjamo96@live.com>
2015-10-05 13:53:22 -0600

    Ahorcado Game!
    Variables

---
+++ Ahorcado game/src/ahorcado/game/AhorcadoGame.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ahorcado.game;
+
+/**
+ *
+ * @author AARON Jara Molina
+ */
+public class AhorcadoGame {
+    private String[] mospl;
+    private String[] letras;
+    private String[] plcompleta;
+    
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
